91248924a4070337e60ff88bf37eb87fa1b9d662
sebitica <sebastian-gabriel.tica@s.unibuc.ro>
2025-11-24 12:02:46 +0200

    Create database.php

---
+++ database.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+    private $host = 'localhost';
+    private $db_name = '-----'; 
+    private $username = '-----'; 
+    private $password = '-----'; 
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            
+        } catch(PDOException $exception) {
+            echo "Eroare de conexiune la bază de date: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
